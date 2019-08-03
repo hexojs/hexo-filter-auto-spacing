ada92a942d29444b13b48cceeeea46f70b54fe54
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-03 14:52:02 +0930

    test: add unit tests and test coverage to travis

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const should = require('chai').should(); // eslint-disable-line
+
+describe('hexo-filter-auto-spacing', () => {
+  // Tests.
+  it('should add space between CJK and Western characters', () => {
+    return true;
+  });
+});
