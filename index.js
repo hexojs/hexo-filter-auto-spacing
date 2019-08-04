b82a3368f61965196e4d37b2f38fa7ca5f4becf2
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-04 23:19:54 +0930

    fix: only add space to text, not links

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-var pangunode = require('pangunode');
+'use strict';
 
-hexo.extend.filter.register('after_post_render', function(data) {
-  data.title = pangunode(data.title);
-  data.content = pangunode(data.content);
+/* global hexo */
+
+const pangu = require('pangu');
+const cheerio = require('cheerio');
+
+hexo.extend.filter.register('after_render:html', str => {
+  let $ = cheerio.load(str, { decodeEntities: false });
+
+  let result = $.html();
+  let words = [];
+
+  $('p').each((index, element) => {
+    words.push($(element).text());
+  });
+
+  $('h1').each((index, element) => {
+    words.push($(element).text());
+  });
+
+  $('h2').each((index, element) => {
+    words.push($(element).text());
+  });
+
+  $('h3').each((index, element) => {
+    words.push($(element).text());
+  });
+
+  $('span').each((index, element) => {
+    words.push($(element).text());
+  });
+
+  words = Array.from(new Set(words));
+
+  for (let index in words) {
+    let word = words[index];
+
+    result = result.replace(word, pangu.spacing(word));
+  }
+
+  return result;
 });
